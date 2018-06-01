8714ef593a2890898aced38f50d2207eb1b5b4e2
iMoeNya <sym9688@outlook.com>
2018-06-01 17:18:35 +0800

    update QRCode

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var img = document.getElementById("showimg");
 
 function random(){
-     var number = parseInt(1000 * Math.random()) % 5;
+     var number = parseInt(1000 * Math.random()) % 6;
      img.src = "./img/image" + number + ".JPG";
      console.log(number);
 }
